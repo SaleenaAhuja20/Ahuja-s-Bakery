ddb194fc253bd8e91fc5ca3e78d3d0f9d49e261a
SaleenaAhuja20 <saleenaahuja038@gmail.com>
2025-02-21 14:13:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ let subject = document.querySelector("#subject");
 let message = document.querySelector("#message");
 
 msgBtn.addEventListener("click", () => {
+
     if(userName.value === "" || email.value === "" || subject.value === ""|| message.value === ""){
-        alert("please fill the information");
-} else{
-    
-    alert("you message has been sent");
+        alert("⚠️ Please fill out all the fields.");
+    }else if (!email.value.includes("@")){
+        alert("⚠️ Email must contain '@'.");
+    }else if (!email.value.endsWith("@gmail.com")){
+        alert("⚠️ Please enter a valid Gmail address (example@gmail.com).");
+    }else{
+        alert("✅ Message sent successfully!");
 }
 });
