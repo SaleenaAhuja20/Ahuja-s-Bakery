5aeb872a3a6af08aa12b8bb2d9a057dbad3892b8
SaleenaAhuja20 <saleenaahuja038@gmail.com>
2025-02-21 13:11:41 +0500

    js starting

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let buttonMsg = document.querySelector(".submit-btn");
-console.dir(buttonMsg);
-let btn = buttonMsg.innerText+" hehehe";
-console.log(btn);
+let msgBtn = document.querySelector(".submit-btn");
+let userName = document.querySelector("#name");
+let email = document.querySelector("#email");
+let subject = document.querySelector("#subject");
+let message = document.querySelector("#message");
+
+userName = null;
+email = null;
+subject = null;
+message = null;
+msgBtn.addEventListener("click", () => {
+    if(userName != null && email != null && subject != null && message != null){
+    alert("you message has been sent");
+} else{
+    alert("please fill the information");
+}
+});
